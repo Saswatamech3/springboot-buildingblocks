87149b28388d2b87eef491776abb7074a98d0e40
Saswata Chakraborty <Saswatamech3@gmail.com>
2021-04-19 23:01:57 -0400

    JPA Project Base

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO TEMP values(101,'Chak@gmail.com','Chak','Chakraborty','Manager','888888888','Chak');
+INSERT INTO TEMP values(102,'Bak@gmail.com','Bak','Chakraborty','Manager','777777777','Bak');
+INSERT INTO TEMP values(100,'Saswata@gmail.com','Saswata','Chakraborty','Manager','999999999','Sak');
